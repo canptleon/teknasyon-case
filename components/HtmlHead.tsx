ffc9cbc8d8ead3739f6884c72ad9c9c545d58fd0
Arda Keyişoğlu <ardakeyisoglu@gmail.com>
2024-11-27 23:16:49 +0300

    meta description fix

--- components/HtmlHead.tsx
+++ components/HtmlHead.tsx
@@ -11,7 +11,7 @@ function HtmlHead(props: Props) {
   return (
     <Head>
       <title>{title}</title>
-      <meta name="description" content="Welcome to my case."/>
+      <meta name="description" content="Welcome to my Teknasyon Frontend Case."/>
       <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
 
       <link rel="icon" href={`/favicon.ico`} />
