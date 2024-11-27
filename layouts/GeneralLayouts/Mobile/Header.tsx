28d7699ed954f8288fb04322659b0e0d55b17588
Arda Keyişoğlu <ardakeyisoglu@gmail.com>
2024-11-27 22:16:22 +0300

    Mobile header relative absoulte css fix.

--- layouts/GeneralLayouts/Mobile/Header.tsx
+++ layouts/GeneralLayouts/Mobile/Header.tsx
@@ -6,7 +6,7 @@ function MobileHeader() {
       <h1 className="mx-[auto] my-[20px] text-center text-[16px] font-semibold">
         Konva Masking and Drawing Tool
       </h1>
-      <p className="max-w-[92%] mx-[auto] my-[10px] text-center text-[13px] mb-[36px]">
+      <p className="max-w-[92%] mx-[auto] my-[10px] text-center text-[13px] mb-[85px]">
         A web-based image manipulation tool built with React and Konva.js, allowing users to upload
         images, draw shapes (Brush, Lasso, Rectangle), and export masked images in PNG, JPG, or JPEG
         formats.
